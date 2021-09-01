9921563a029834f890fb24cdee7ffc8c0f683b73
flofsid <64130804+flofsid@users.noreply.github.com>
2021-09-01 09:14:16 +0200

    Add files via upload

---
+++ Region.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EA_TypenCreator
+{
+    class Region
+    {
+        #region ****************** Parameter and Values ******************
+
+        #endregion
+
+        #region ****************** Init and ctors ******************
+
+        #endregion
+
+        #region ****************** Functions ******************
+
+        #endregion
+    }
+}
